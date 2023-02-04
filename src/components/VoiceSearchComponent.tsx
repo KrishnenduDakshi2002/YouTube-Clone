a34339726f2085d79b29539891156f729e23b6ba
krishnendu Dakshi <kdakshi2020@gamil.com>
2023-02-05 02:03:23 +0530

    added speech to text in voice search

--- src/components/VoiceSearchComponent.tsx
+++ src/components/VoiceSearchComponent.tsx
@@ -1,53 +1,110 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { FaMicrophone } from 'react-icons/fa'
-import { IoClose } from 'react-icons/io5'
-import TestVideo from '../assets/Video/test-video.mp4'
+import React, { useEffect, useRef, useState } from "react";
+import { FaMicrophone, FaMicrophoneSlash } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
 
-
-function VoiceSearchComponent({setIsVisibleVoiceSearch}:{setIsVisibleVoiceSearch:()=>void}) {
-    const AudioRef = useRef<HTMLAudioElement|null>(null);
-    const [isMicroPhoneActive, setIsMicroPhoneActive] = useState(true);
-    useEffect(() => {
-        navigator.mediaDevices
-        .getUserMedia({ video: false, audio: true })
-        .then((stream) => {
-            if(AudioRef.current == null)return;
-            // getting the first audio track
-            const AudioTrack = stream.getAudioTracks()[0];
-            
-            AudioRef.current.srcObject = stream;
-            AudioRef.current.autoplay = true;
-        })
-        .catch((err) => {
-          console.error(`you got an error: ${err}`);
-        });
-    }, [])
-    
-  return (
-    <div className='fixed top-2 bottom-0 right-0 left-0 z-[110] bg-[rgba(0,0,0,0.5)] flex justify-center' onClick={(e)=>{
-        setIsVisibleVoiceSearch();
-    }}>
-        <div className="bg-[#212121] w-[40rem] h-[28rem] flex flex-col rounded-xl p-5 text-white" onClick={e=>e.stopPropagation()}>
-            <div className="flex-3 flex justify-end">
-                <button onClick={()=>setIsVisibleVoiceSearch()} >
-                    <IoClose  size={30}/>
-                </button>
-            </div>
-            <div className='text-3xl flex-1'>
-                {/* <p>Listening...</p>
-                <p>Microphone off. Try again</p> */}
-                <audio ref={AudioRef} controls></audio>
-
-            </div>
-            <div className="flex-1  flex flex-col justify-center items-center">
-                <button className='w-20 h-20 rounded-full bg-red-700 flex justify-center items-center cursor-pointer'>
-                    <FaMicrophone size={30}/>
-                </button>
-                <p className='text-sm text-gray-400'>Tap the microphone to try again</p>
-            </div>
-        </div>
-    </div>
-  )
+declare global {
+  interface Window {
+    SpeechRecognition: any;
+    webkitSpeechRecognition: any;
+  }
 }
 
-export default VoiceSearchComponent
+function VoiceSearchComponent({
+  setIsVisibleVoiceSearch,
+}: {
+  setIsVisibleVoiceSearch: () => void;
+}) {
+  //   Hooks
+
+  const [isMicroPhoneActive, setIsMicroPhoneActive] = useState(true);
+  const SearchTextAreaRef = useRef<HTMLParagraphElement | null>(null);
+
+
+  // Speech recognition
+  window.SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  let speechRecognition = new window.SpeechRecognition();
+  speechRecognition.interimResults = true;
+  speechRecognition.lang = "en-US";
+
+  speechRecognition.addEventListener("result", (e: any) => {
+    const searchString = e.results[0][0].transcript;
+    console.log(e.results[0][0].transcript);
+    if(SearchTextAreaRef.current){
+        SearchTextAreaRef.current.innerText = searchString;
+    }
+  });
+
+  function HandleSessionEnd(){
+    console.log("session restarting");
+    speechRecognition.start();
+  }
+
+  useEffect(()=>{
+    // adding end event listner when mounting
+    console.log('mounting')
+    speechRecognition.addEventListener("end", HandleSessionEnd);
+
+    return ()=>{
+        console.log('unmounting')
+        // on unmounting it will remove the event listener which has been add while mounting
+        speechRecognition.removeEventListener('end',HandleSessionEnd);
+    }
+  })
+
+  return (
+    <div
+      className="fixed top-2 bottom-0 right-0 left-0 z-[110] bg-[rgba(0,0,0,0.5)] flex justify-center"
+      onClick={(e) => {
+        setIsVisibleVoiceSearch();
+      }}
+    >
+      <div
+        className="bg-[#212121] w-[40rem] h-[28rem] flex flex-col rounded-xl p-5 text-white"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="flex-3 flex justify-end">
+          <button onClick={() => {setIsVisibleVoiceSearch()}}>
+            <IoClose size={30} />
+          </button>
+        </div>
+        <div className="text-3xl flex-1">
+          {isMicroPhoneActive ? (
+            <p ref={SearchTextAreaRef}>Listening...</p>
+          ) : (
+            <p>Microphone off. Try again</p>
+          )}
+        </div>
+        <div className="flex-1  flex flex-col justify-center items-center">
+          {isMicroPhoneActive ? (
+            <button
+              className="w-20 h-20 rounded-full bg-red-700 flex justify-center items-center cursor-pointer"
+              onClick={() => {
+                // setIsMicroPhoneActive(false);
+                speechRecognition.start();
+              }}
+            >
+              <FaMicrophone size={30} />
+            </button>
+          ) : (
+            <button
+              className="w-20 h-20 rounded-full bg-red-700 flex justify-center items-center cursor-pointer"
+              onClick={() => {
+                // setIsMicroPhoneActive(true);
+                // speechRecognition.start();
+              }}
+            >
+              <FaMicrophoneSlash size={30} />
+            </button>
+          )}
+          <p className="text-sm text-gray-400">
+            Tap the microphone to try again
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default VoiceSearchComponent;
